c5ec525fa0553cb6996aff30902d117b98c059ff
dmzazz <dimaslaylaa@gmail.com>
2023-04-27 01:46:07 +0700

    fix app.js

--- frontend_stocktifity/src/App.js
+++ frontend_stocktifity/src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
-import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Import Components
 import Login from "./components/Login";
@@ -17,52 +16,28 @@ import { OrderProvider } from "./context/OrderContext";
 import Header from "./pages/Header";
 
 const App = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(
-    localStorage.getItem("token") !== null
-  );
-
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    setIsAuthenticated(false);
-  };
-
   return (
     <Router>
       <OrderProvider>
         <InventoryProvider>
           <SupplierProvider>
             <Routes>
-              {/* Route Login */}
-              <Route
-                path="Login"
-                element={
-                  isAuthenticated ? (
-                    <Navigate to="/" />
-                  ) : (
-                    <Login setIsAuthenticated={setIsAuthenticated} />
-                  )
-                }
-              />
+              <Route path="Login" element={<Login />} />
 
-              {/* Route Dashboard */}
               <Route
                 path="/"
-                element={
-                  isAuthenticated ? (
-                    <Sidebar handleLogout={handleLogout}>
-                      <Dashboard />
-                    </Sidebar>
-                  ) : (
-                    <Navigate to="/Login" />
-                  )
+                element={<Sidebar />}
+                children={
+                  <>
+                    <Route path="/" element={<Dashboard />} />
+                    <Route path="Dashboard" element={<Dashboard />} />
+                    <Route path="Inventory" element={<Inventory />} />
+                    <Route path="Order" element={<Order />} />
+                    <Route path="Supplier" element={<Supplier />} />
+                    <Route path="Header" element={<Header/>}/>
+                  </>
                 }
-              >
-                <Route path="/" element={<Dashboard />} />
-                <Route path="Dashboard" element={<Dashboard />} />
-                <Route path="Inventory" element={<Inventory />} />
-                <Route path="Order" element={<Order />} />
-                <Route path="Supplier" element={<Supplier />} />
-              </Route>
+              />
             </Routes>
           </SupplierProvider>
         </InventoryProvider>
